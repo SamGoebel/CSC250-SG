e983be95240b03c5731f2f3398b1245e894f1c32
SamGoebel <zxtra01@gmail.com>
2022-04-03 21:36:43 -0500

    hw 12
    added walls

---
+++ My project/Assets/Movement.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   public float speed = 0.1f;
+
+ 
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(xDirection, 0f, zDirection);
+        transform.position += moveDirection * speed;
+
+    }
+}
